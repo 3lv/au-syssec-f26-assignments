dc790bd254f47c22e1269da9c0872749ad8d409d
3lv <cristian.vlad.vladutu@gmail.com>
2026-03-07 00:01:57 +0100

    Working without encryption

--- network/task1/src/client.c
+++ network/task1/src/client.c
@@ -2,7 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+uint16_t checksum(const void *icmp, size_t total_size) {
+    const uint8_t *bytes = (const uint8_t *)icmp;
+    uint32_t sum = 0;
+
+    if (total_size % 2 == 0) {
+        // Error coaie
+        //return NULL;
+    }
+    
+    for (size_t i = 0; i < total_size; i += 2) {
+        uint16_t word = ((uint16_t)bytes[i] << 8) + bytes[i+1];
+        sum += word;
+    }
+
+    // Ones complement
+    while (sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+
+    return (uint16_t)(~sum);
+}
+
+uint8_t* build_icmp_custom_format(uint8_t type, uint8_t code, const uint8_t* content, size_t content_size) {
+    size_t total_size = 4*2 + content_size;
+    uint8_t *icmp_payload = (uint8_t *)malloc((total_size));
+    memset(icmp_payload, 0, total_size);
+    memcpy(icmp_payload, &type, 1);
+    memcpy(icmp_payload+1, &code, 1);
+    memcpy(icmp_payload+4*2, content, content_size);
+    uint16_t cs = checksum(icmp_payload, total_size);
+    // Was in low endian by default
+    uint8_t msb = (cs >> 8) & 0xFF;
+    uint8_t lsb = cs & 0xFF;
+    memcpy(icmp_payload+2, &msb, 1);
+    memcpy(icmp_payload+3, &lsb, 1);
+    return icmp_payload;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <destination IP>\n", argv[0]);
+        return 1;
+    }
     char errbuf[LIBNET_ERRBUF_SIZE];
 
     libnet_t *l = libnet_init(LIBNET_RAW4, NULL, errbuf);
@@ -11,8 +53,9 @@ int main(void) {
         return 1;
     }
 
+    // When now on the same host, use src_ip 192.168.1.1
     uint32_t src_ip = libnet_name2addr4(l, "192.168.1.10", LIBNET_RESOLVE);
-    uint32_t dst_ip = libnet_name2addr4(l, "192.168.1.1", LIBNET_RESOLVE);
+    uint32_t dst_ip = libnet_name2addr4(l, argv[1], LIBNET_RESOLVE);
 
     if (src_ip == (uint32_t)-1 || dst_ip == (uint32_t)-1) {
         fprintf(stderr, "Failed to resolve IP address\n");
@@ -20,33 +63,45 @@ int main(void) {
         return 1;
     }
 
-    libnet_ptag_t ip = libnet_build_ipv4(
-        LIBNET_IPV4_H + 0,   // total length
-        0,                   // TOS
-        0x1234,              // ID
-        0,                   // fragmentation
-        64,                  // TTL
-        IPPROTO_RAW,         // protocol
-        0,                   // checksum, autofill
-        src_ip,
-        dst_ip,
-        NULL,                // payload
-        0,                   // payload size
-        l,
-        0
-    );
+    while (1) {
+        size_t sml = 32;
+        uint8_t *sm = (uint8_t *)malloc(sml * 1);
+        //Read from stdin
+        printf("Enter a message to send: ");
+        fgets((char *)sm, sml, stdin);
+        //strcpy(sm, "Secret message");
+        uint8_t *icmp = build_icmp_custom_format(47, 0, sm, sml);
+        uint8_t icmpl = sml + 4*2;
 
-    if (ip == -1) {
-        fprintf(stderr, "libnet_build_ipv4 failed: %s\n", libnet_geterror(l));
-        libnet_destroy(l);
-        return 1;
-    }
+        libnet_ptag_t ip = libnet_build_ipv4(
+            LIBNET_IPV4_H + icmpl,   // total length
+            0,                   // TOS
+            0x1234,              // ID
+            0,                   // fragmentation
+            64,                  // TTL
+            //IPPROTO_RAW,         // protocol
+            1, // Protocol ICMP
+            0,                   // checksum, autofill
+            src_ip,
+            dst_ip,
+            icmp,                // payload
+            icmpl,                   // payload size
+            l,
+            0
+        );
 
-    int bytes = libnet_write(l);
-    if (bytes == -1) {
-        fprintf(stderr, "libnet_write failed: %s\n", libnet_geterror(l));
-    } else {
-        printf("Injected %d bytes\n", bytes);
+        if (ip == -1) {
+            fprintf(stderr, "libnet_build_ipv4 failed: %s\n", libnet_geterror(l));
+            libnet_destroy(l);
+            return 1;
+        }
+
+        int bytes = libnet_write(l);
+        if (bytes == -1) {
+            fprintf(stderr, "libnet_write failed: %s\n", libnet_geterror(l));
+        } else {
+            printf("Injected %d bytes\n", bytes);
+        }
     }
 
     libnet_destroy(l);
--- network/task1/src/server.c
+++ network/task1/src/server.c
@@ -14,6 +14,16 @@ void print_all_devs(pcap_if_t *alldevs) {
     }
 }
 
+void display_data(const uint8_t *data, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%02x ", data[i]);
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
 int main(void) {
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_if_t *alldevs = NULL;
@@ -33,12 +43,20 @@ int main(void) {
     }
 
     struct pcap_pkthdr *header;
-    const unsigned char *data;
+    const uint8_t *data;
     int rc = pcap_next_ex(handle, &header, &data);
+    // find first icmpv4 and type 47
+    while (rc == 1) {
+        // Add the 14 for the Ethernet header and 9 to get to protocol in ip header
+        // 20 is the ip header size
+        if (header->len >= 34 && data[14 + 9] == 1 && data[14 + 20 + 0] == 47) {
+            char *message = (char *)(data + 14 + 20 + 4*2);
+            printf("Received message: %s\n", message);
+        }
+        rc = pcap_next_ex(handle, &header, &data);
+    }
 
-    if (rc == 1) {
-        printf("Captured packet of length %u bytes\n", header->len);
-    } else if (rc == 0) {
+    if (rc == 0) {
         printf("Timeout waiting for packet\n");
     } else {
         fprintf(stderr, "pcap_next_ex failed: %s\n", pcap_geterr(handle));
